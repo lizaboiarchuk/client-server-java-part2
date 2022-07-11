84ee17406b00c9ad5a015ed27c014c2034b4677d
lizaboiarchuk <yelyzaveta.boiarchuk@gmail.com>
2022-07-11 15:49:24 +0300

    lab4 upd

---
+++ src/main/java/com/labs/lab4/Main.java
@@ -0,0 +1,57 @@
+package com.labs.lab4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "user", "PASSWORD");
+            Service service = new Service(conn);
+            service.createTable();
+            service.clearTable();
+            printProducts(service.readProducts());
+
+            // create products
+            service.createProduct("Milk", 3, 25.99, "Dairy");
+            service.createProduct("Tomato", 200, 34.99, "Grocery");
+            service.createProduct("Cucumber", 3, 32.99, "Grocery");
+            service.createProduct("Carrot", 50, 22.99, "Grocery");
+            printProducts(service.readProducts());
+
+
+            // update product
+            Product newTomato = new Product(2, "Tomato", 99,34.99, "Grocery");
+            service.updateProduct(newTomato);
+            printProducts(service.readProducts());
+
+
+            // delete product
+            service.deleteProduct(4);
+            printProducts(service.readProducts());
+
+
+            // select with filters
+            Map<String,Object> filters = new HashMap<String, Object>(){{ put("amount", 3); }};
+            System.out.println("\nSelected products: ");
+            for (var p: service.listByCriteria(filters)) {
+                System.out.println(p);
+            }
+
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    public static void printProducts(List<Product> products) {
+        System.out.println("\nCurrent products: ");
+        for (var p: products) {
+            System.out.println(p);
+        }
+        System.out.println("-----");
+    }
+
+}
---
+++ src/main/java/com/labs/lab4/Product.java
@@ -0,0 +1,45 @@
+package com.labs.lab4;
+
+public class Product {
+
+    private int id;
+    private String title;
+    private int amount;
+    private double price;
+    private String category;
+
+
+    Product(int id, String name, int amount, double price, String group) {
+        this.id = id;
+        this.title = name;
+        this.amount = amount;
+        this.price = price;
+        this.category = group;
+    }
+
+    public void setId(int id) { this.id = id; }
+    public int getId() { return this.id; }
+
+    public void setTitle(String title) { this.title = title; }
+    public String getTitle() { return this.title; }
+
+    public void setCategory(String category) { this.category = category; }
+    public String getCategory() { return category; }
+
+    public void setPrice(double price)  { this.price = price; }
+    public double getPrice() { return price; }
+
+    public void setAmount(int amount) { this.amount = amount; }
+    public int getAmount() { return amount; }
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", amount=" + amount +
+                ", group='" + category +  '}';
+    }
+}
